a3a29569e60075fd07373c63a458bbc814b2543f
Florencia Averame Kramer <62817003+FlorenciaAK@users.noreply.github.com>
2020-05-12 18:39:17 -0300

    título da janela

--- codigo.py
+++ codigo.py
@@ -5,5 +5,11 @@ from config import WIDTH, HEIGHT, INIT, GAME, QUIT
 from init_screen import init_screen
 from game_screen import game_screen
 
-#inicialização do pacote pygame
+# inicio pacote do pygame
 pygame.init()
+
+#cria tela com 500 pixels de largura e 400 pixels de altura
+pygame.display.set_mode((500,400))
+
+#define o título da janela
+pygame.display.set_caption ("Cara a cara")
