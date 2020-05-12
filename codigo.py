a6e4b6cb865f896e26f9934c442ae21416b8c409
Florencia Averame Kramer <62817003+FlorenciaAK@users.noreply.github.com>
2020-05-12 18:32:40 -0300

    importações

--- codigo.py
+++ codigo.py
@@ -1,4 +1,9 @@
+#importações necessárias
 import pygame
+import random
+from config import WIDTH, HEIGHT, INIT, GAME, QUIT
+from init_screen import init_screen
+from game_screen import game_screen
 
-# inicio pacote do pygame
+#inicialização do pacote pygame
 pygame.init()
