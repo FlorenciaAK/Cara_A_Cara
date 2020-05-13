e65568a5be63045488329f9565e18e53e929b699
Florencia Averame Kramer <62817003+FlorenciaAK@users.noreply.github.com>
2020-05-13 16:16:30 -0300

    importações

--- codigo.py
+++ codigo.py
@@ -1,15 +1,13 @@
 #importações necessárias
 import pygame
 import random
-from config import WIDTH, HEIGHT, INIT, GAME, QUIT
-from init_screen import init_screen
-from game_screen import game_screen
+
 
 # inicio pacote do pygame
 pygame.init()
 
 #cria tela com 500 pixels de largura e 400 pixels de altura
-pygame.display.set_mode((500,400))
+pygame.display.set_mode((500,300))
 
 #define o título da janela
 pygame.display.set_caption ("Cara a cara")
